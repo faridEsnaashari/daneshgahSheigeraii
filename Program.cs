dd09181ba2679b3fc9e14191e76a9810a49201a3
faridEsnaashari <faride13578642@gmail.com>
2021-04-10 19:25:29 +0430

    create a new project with dotnet cli

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace gameExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
