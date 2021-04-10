f3b22e2e2aee7911bf2ee1cffdaa254f05af6b26
faridEsnaashari <faride13578642@gmail.com>
2021-04-10 22:24:11 +0430

    create some object for test

--- Program.cs
+++ Program.cs
@@ -6,7 +6,19 @@ namespace gameExample
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Player player = new Player();
+
+            // Movable objectes
+            Enemy soldier1 = new Enemy();
+            Enemy soldier2 = new Enemy();
+
+            MovableLaser laser1 = new MovableLaser();
+
+            // Movable objectes
+            UnMovableLaser laser2 = new UnMovableLaser();
+
+            Trap trap1 = new Trap();
+            Trap trap2 = new Trap();
         }
     }
 }
