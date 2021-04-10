19556dc78fb33733de99bf1a7faa476475592777
faridEsnaashari <faride13578642@gmail.com>
2021-04-10 22:11:59 +0430

    Unmovable concrete class

---
+++ classes/FailureReason/UnMovable.cs
@@ -0,0 +1,22 @@
+using System;
+
+class UnMovable: FailureReason
+{
+    public override string failureType 
+    {
+        set { _failureType = value; }
+        get { return _failureType; }
+    }
+
+    public override int damage 
+    {
+        set { _damage = value; }
+        get { return _damage; }
+    }
+
+    public void defuse()
+    {
+        Console.WriteLine("defused");
+    }
+}
+
