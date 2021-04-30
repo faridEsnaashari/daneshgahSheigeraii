683a2d6cb5d1acef980cae5eab8bd915a71b2be2
faridEsnaashari <faride13578642@gmail.com>
2021-04-30 11:53:15 +0430

    changed due to new diagram

--- Program.cs
+++ Program.cs
@@ -12,8 +12,6 @@ namespace gameExample
             Customer customer1 = new Customer();
             Customer customer2 = new Customer();
             Customer customer3 = new Customer();
-            Customer customer4 = new Customer();
-            Customer customer5 = new Customer();
 
             bank.addCustomer(customer1);
             bank.addCustomer(customer2);
@@ -30,25 +28,17 @@ namespace gameExample
 
 
             Console.WriteLine("\n\n######### ADDING EMPLOYES #########");
-            Employe employe1 = new Employe();
-            Employe employe2 = new Employe();
-            Employe employe3 = new Employe();
-
-            bank.addEmploye(employe1);
-            bank.addEmploye(employe2);
-            bank.addEmploye(employe1);
-            bank.addEmploye(employe3);
+            bank.addEmploye();
+            bank.addEmploye();
+            bank.addEmploye();
+            bank.addEmploye();
 
 
             Console.WriteLine("\n\n######### ADDING ATMS #########");
-            ATM atm1 = new ATM();
-            ATM atm2 = new ATM();
-            ATM atm3 = new ATM();
-
-            bank.addAtm(atm1);
-            bank.addAtm(atm2);
-            bank.addAtm(atm3);
-            bank.addAtm(atm3);
+            bank.addAtm();
+            bank.addAtm();
+            bank.addAtm();
+            bank.addAtm();
         }
 
     }
